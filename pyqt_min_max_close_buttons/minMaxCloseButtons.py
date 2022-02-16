53c5012b6d3b76fda0fd8003eb48637585d9087f
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-16 21:11:13 +0900

    First commit from local

---
+++ pyqt_min_max_close_buttons/minMaxCloseButtons.py
@@ -0,0 +1,44 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton
+
+
+class MinMaxCloseButtonsWidget(QWidget):
+    def __init__(self, hint=Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint):
+        super().__init__()
+        self.__initVal()
+        self.__initUi(hint)
+
+    def __initVal(self):
+        self._closeBtn = QPushButton()
+        self._minimizeBtn = QPushButton()
+        self._maximizeBtn = QPushButton()
+
+    def __initUi(self, hint):
+        lay = QHBoxLayout()
+        lay.setContentsMargins(0, 0, 0, 0)
+        lay.setSpacing(2)
+
+        if hint == Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint:
+            lay.addWidget(self._minimizeBtn)
+            lay.addWidget(self._maximizeBtn)
+            lay.addWidget(self._closeBtn)
+        elif hint == Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint:
+            lay.addWidget(self._minimizeBtn)
+            lay.addWidget(self._closeBtn)
+        elif hint == Qt.WindowCloseButtonHint:
+            lay.addWidget(self._closeBtn)
+        else:
+            # todo for another type of flags
+            pass
+
+        self.setLayout(lay)
+
+    def getMinimizedBtn(self):
+        return self._minimizeBtn
+
+    def getMaximizedBtn(self):
+        return self._maximizeBtn
+
+    def getCloseBtn(self):
+        return self._closeBtn
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-min-max-close-buttons-widget',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt modern min/max/close buttons widget for title bar or menu bar',
+    url='https://github.com/yjg30737/pyqt-min-max-close-buttons-widget.git',
+    install_requires=[
+        'PyQt5>=5.8'
+    ]
+)
