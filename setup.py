e5d565728c1364a39169bb68abf0207f14675cbe
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-14 15:14:37 +0900

    Add icons

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
+    package_data={'pyqt_min_max_close_buttons_widget.ico': ['full_screen.svg', 'help.svg', 'tack.svg',
+                                                            'fold.svg', 'unfold.svg']},
     description='PyQt modern min/max/close buttons widget for title bar or menu bar',
     url='https://github.com/yjg30737/pyqt-min-max-close-buttons-widget.git',
     install_requires=[
