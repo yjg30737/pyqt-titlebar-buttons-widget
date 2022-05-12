baa7537e048bb2af36666eb6b9d655c68b2aa47d
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 11:20:23 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-titlebar-buttons-widget',
-    version='0.0.2',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -11,6 +18,8 @@ setup(
                                                             'fold.svg', 'unfold.svg']},
     description='PyQt buttons (e.g. min/max/close) widget for title bar or menu bar',
     url='https://github.com/yjg30737/pyqt-titlebar-buttons-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-svg-icon-pushbutton>=0.0.1'
